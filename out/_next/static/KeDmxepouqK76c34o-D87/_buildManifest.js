e6a2afe9e129bb70b495869f277105d668d9f582
sungman5 <sungman5@gmail.com>
2023-01-24 16:47:42 +0900

    deploy to gh-pages

---
+++ out/_next/static/KeDmxepouqK76c34o-D87/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ad27f43ef013a73b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/components/nav":["static/chunks/pages/components/nav-831700e2b79e488c.js"],sortedPages:["/","/_app","/_error","/components/nav"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
